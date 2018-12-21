0ba96e8408d47551decba8ad233ca637ac20456c
hectortorresvicente <38098502+hectortorresvicente@users.noreply.github.com>
2018-12-21 12:07:29 +0100

    post de UsuarioEM

--- SuenosHTVokBeta/src/main/java/com/htv/api/UsuariosAPI.java
+++ SuenosHTVokBeta/src/main/java/com/htv/api/UsuariosAPI.java
@@ -3,14 +3,18 @@ package com.htv.api;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.htv.models.Usuario;
 import com.htv.persistencia.UsuarioEM;
+import com.mindty.persistence.CoordinadorEM;
 
 
 
@@ -37,4 +41,19 @@ public class UsuariosAPI {
 	}
 	};
 
+	@Path("")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@POST
+	//Codigo POST
+	public Response addUsuario(Usuario nuevoUsuario) {
+		try {
+			
+			return Response.status(202).entity(UsuarioEM.getInstance().NuevoUsuario(nuevoUsuario)).build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR.getStatusCode())
+					.entity("Ha habido un error al guardar el pedido").build();
+		}		
+	}
 }
